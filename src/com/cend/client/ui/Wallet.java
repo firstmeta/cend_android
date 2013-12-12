cd6e872ff12ed3cb73660304b1c34af9ceffa37e
Reza <reza@firstmeta.com>
2013-12-12 10:30:19 +0800

    Wallet activity cleanup

--- src/com/cend/client/ui/Wallet.java
+++ src/com/cend/client/ui/Wallet.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.jivesoftware.smack.util.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.ComponentName;
@@ -52,40 +51,7 @@ public class Wallet extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
 		setContentView(R.layout.wallet);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-//		JSONArray currArray = new JSONArray();
-//		JSONObject imvu = new JSONObject();
-//		JSONObject lindens = new JSONObject();
-//		JSONObject usd = new JSONObject();
-//		JSONObject euro = new JSONObject();
-//		try {
-//			imvu.put("id", 112);
-//			imvu.put("name", "IMVU CREDITS");
-//			imvu.put("balance", 22000);
-//			lindens.put("id", 122);
-//			lindens.put("name", "LINDENS");
-//			lindens.put("balance", 12000);
-//			usd.put("id", 231);
-//			usd.put("name", "USD");
-//			usd.put("balance", 22);
-//			euro.put("id", 22121);
-//			euro.put("name", "EURO");
-//			euro.put("balance", 20);
-//		} catch (JSONException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		currArray.put(imvu);
-//		currArray.put(lindens);
-//		currArray.put(usd);
-//		currArray.put(euro);
-//		ArrayList<WalletCurrency> currencies = new ArrayList<WalletCurrency>();
-//		currencies.addAll(WalletCurrency.fromJson(currArray));
-//		WalletCurrenciesAdapter adapter = new WalletCurrenciesAdapter(this, 0);
-//		adapter.addAll(currencies);
-//		ListView lv = (ListView) findViewById(R.id.walletListView);
-//		lv.setAdapter(adapter);
-		
+		getActionBar().setDisplayHomeAsUpEnabled(true);		
 	}
 	
 	@Override
@@ -134,6 +100,7 @@ public class Wallet extends Activity {
 		return true;
 	}
 
+	@Override
 	public final boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.contact_list_menu_settings:
